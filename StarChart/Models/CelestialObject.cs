1dbe0170e2378ade3634f6a9fb495e663137d653
Dost <dost2010@yahoo.com>
2018-12-25 13:23:35 -0700

    Adding new file

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        int? OrbitedObjectId { get; set; }
+        [NotMapped]
+        public List<CelestialObject> Sattellite { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+}
